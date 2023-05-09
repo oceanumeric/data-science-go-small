4607984508bfa68a3f087d96ef4f6618bbd27938
Michael WANG Fei <79036899+oceanumeric@users.noreply.github.com>
2023-05-09 15:46:23 +0000

    [update]

--- Tutorial_01/linear_regression.R
+++ Tutorial_01/linear_regression.R
@@ -1,3 +1,37 @@
 library(MASS)
 
+# Load the Boston housing data
+data(Boston)
+str(Boston)
+names(Boston)
+
+# meaning of the variables
 ?Boston
+
+# plot the data
+plot(Boston)
+
+# plot median value against status
+options(repr.plot.width = 8, repr.plot.height = 6)
+plot(medv ~ lstat, data = Boston,
+            xlab = "Lower status of the population",
+            ylab = "Median value of owner-occupied homes in $1000s")
+
+# fit a linear regression model
+fit <- lm(medv ~ lstat, data = Boston)
+summary(fit)
+
+# plot the fitted model
+options(repr.plot.width = 8, repr.plot.height = 6)
+plot(medv ~ lstat, data = Boston,
+            xlab = "Lower status of the population",
+            ylab = "Median value of owner-occupied homes in $1000s")
+abline(fit, col = "red")
+
+
+# export to a table
+
+# install.packages("stargazer")
+library(stargazer)
+stargazer(fit, type = "text")
+stargazer(fit, type = "html")
